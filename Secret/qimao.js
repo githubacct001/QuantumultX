1dcb98e205e532eeeafc74ae0c6d0cac368e8c76
Carl Quinn <63450369+githubacct001@users.noreply.github.com>
2021-03-27 20:17:40 +0800

    Create qimao.js

---
+++ Secret/qimao.js
@@ -0,0 +1,20 @@
+/* 
+本脚本仅用作学习之用
+脚本归属权由 @githubacct001 所有
+请于下载此脚本的24内删除本脚本
+否则后果自负
+
+[mitm]
+hostname = xiaoshuo.wtzw.com
+
+*/
+
+let obj = JSON.parse($response.body)
+
+obj.value.vip = ｛
+  "data" : {
+    "is_vip" : "1",
+  }
+};
+  
+$done({body: JSON.stringify(obj)});
