baeb8d7aaca9781cc391f17ca7454cb88c55229b
Carl Quinn <63450369+githubacct001@users.noreply.github.com>
2021-04-14 19:54:29 +0800

    Update qimao.js

--- qimao.js
+++ qimao.js
@@ -1,3 +1,15 @@
+/*
+[mitm]
+hostname = *.wtzw.com
+
+[rewrite]
+^https:\/\/xiaoshuo\.wtzw\.com\/api\/v(1|2)\/user url script-response-body https://raw.githubusercontent.com/githubacct001/QuantumultX/secret/qimao.js
+*/
+
+# 底部横幅广告
+^https:\/\/api-ks\.wtzw\.com\/api\/v(1|2)\/(float-adv|init-adv|reader-adv) url reject
+
+
 let obj = JSON.parse($response.body);
 
 obj.data["is_vip"] = "1";
