eb003ed67a29f850da18c22dda25da702b4eefeb
Carl Quinn <63450369+githubacct001@users.noreply.github.com>
2022-03-27 21:17:32 +0800

    Update qimao.js

--- qimao.js
+++ qimao.js
@@ -3,11 +3,19 @@
 hostname = xiaoshuo.wtzw.com
 
 [rewrite]
-^https:\/\/xiaoshuo\.wtzw\.com\/api\/v(1|2)\/user url script-response-body https://raw.githubusercontent.com/githubacct001/QuantumultX/secret/qimao.js
+^https:\/\/xiaoshuo\.wtzw\.com\/api\/v(1|2|3)\/user url script-response-body https://raw.githubusercontent.com/githubacct001/QuantumultX/secret/qimao.js
 */
 
-let obj = JSON.parse($response.body);
+var url = $request.url;
+var obj = JSON.parse($response.body);
+const v12 = "/api/v(1|2)/user";
+const v3 = "/api/v3/user";
 
-obj.data["is_vip"] = "1";
+if (url.indexOf(v12) != -1) {
+    obj.data["is_vip"] = "1";
+}
+if (url.indexOf(v3) != -1) {
+    obj.data["user_area"]["base_info"]["is_vip"] = "1";
+}
 
 $done({body: JSON.stringify(obj)});
