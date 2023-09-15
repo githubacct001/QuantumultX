add65fbf362e851cdac3440cca1b80a4afbc2265
Carl Quinn <63450369+githubacct001@users.noreply.github.com>
2023-09-15 20:26:44 +0800

    Create zh_related.js

---
+++ Rewrite/Zhihu/zh_related.js
@@ -0,0 +1,16 @@
+/*
+去知乎相关推荐@lbqmy2022
+
+[rewrite_local]
+^https?:\/\/www\.zhihu\.com\/api\/v4\/answers\/2499001437\/related-readings url script-response-body zh_related.js
+
+[mitm]
+hostname = www.zhihu.com
+
+*/
+
+let obj = JSON.parse($response.body);
+
+obj = {};
+  
+$done({body: JSON.stringify(obj)});
