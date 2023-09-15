c5badd89e8f050b606637db17f636d78cf3c8079
Carl Quinn <63450369+githubacct001@users.noreply.github.com>
2023-09-15 20:43:34 +0800

    Update zh_related.js

--- Rewrite/Zhihu/zh_related.js
+++ Rewrite/Zhihu/zh_related.js
@@ -2,7 +2,7 @@
 去知乎相关推荐@lbqmy2022
 
 [rewrite_local]
-^https?:\/\/www\.zhihu\.com\/api\/v4\/answers\/\d+\/related-readings url script-response-body https://raw.githubusercontent.com/githubacct001/QuantumultX/secret/Rewrite/Zhihu/zh_related.js
+^https?:\/\/www\.zhihu\.com\/api\/v4\/(answers|questions)\/\d+\/related-readings url script-response-body https://raw.githubusercontent.com/githubacct001/QuantumultX/secret/Rewrite/Zhihu/zh_related.js
 
 [mitm]
 hostname = www.zhihu.com
